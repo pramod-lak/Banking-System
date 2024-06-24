89b3c393344a0ae5fd6644706489c1173a20ce94
Pramod Lakshan <lakshanvvr@gmail.com>
2024-06-24 20:13:55 +0530

    Modify & add comments

--- bankSystem.py
+++ bankSystem.py
@@ -1,6 +1,7 @@
 import random
 from datetime import date
 
+#BankAccount class to create bank account objects
 class BankAccount:
     def __init__(self, account_number, name, age, nic, initial_deposit):
         self.account_number = account_number
@@ -9,6 +10,7 @@ class BankAccount:
         self.nic = nic
         self.balance = initial_deposit
 
+    #Deposit method to deposit money to the account
     def deposit(self):
         try:
             while True:
@@ -26,6 +28,7 @@ class BankAccount:
         except:
             print("Invalid input. Please enter a deposit valid amount.")
         
+    #Withdraw method to withdraw money from the account
     def withdraw(self):
         try:
             while True:
@@ -46,9 +49,11 @@ class BankAccount:
         except:
             print("Invalid input. Please enter a valid withdrawal amount.")
 
+    #Check balance method to check the account balance
     def check_balance(self):
         print(f"\nYour current account balance is Rs.{self.balance:.2f}.")
 
+    #Transfer method to transfer money to another account
     def transfer(self, to_account):
         try:
             while True:
@@ -70,6 +75,52 @@ class BankAccount:
         except:
             print("Invalid input. Please enter a valid transfer amount.")
 
+#Function to create a new account
+def new_account_create():
+    while True:
+        name = input("\nEnter your name: ")
+        if name:
+            break
+        else:
+            print("Name can't be empty. Please try again.")
+    while True:
+        age = input("Enter your age: ")
+        if age:
+            break
+        else:
+            print("Age can't be empty. Please try again.")
+    while True:
+        nic = input("Enter your NIC number: ")
+        if nic:
+            break
+        else:
+            print("NIC number can't be empty. Please try again.")
+    try:
+        while True:
+            initial_deposit = float(input("Enter initial deposit amount: "))
+            if initial_deposit >= 1000:
+                break
+            elif 0 < initial_deposit < 1000:
+                print("Initial deposit amount must be at least Rs.1000.00. Please try again.")
+            elif initial_deposit == 0:
+                print("Initial deposit amount can't be zero. Please try again.")
+            elif initial_deposit < 0:
+                print("Initial deposit amount can't be negative. Please try again.")
+        account_number = account_number_generate()
+        account1 = BankAccount(account_number, name, age, nic, initial_deposit)
+        accounts_list.append(account1)
+        print(f"\nAccount created successfully. Your account number is {account1.account_number}.")
+        print("\n---- New Account Details ----")
+        print(f"Account number\t\t: {account1.account_number}")
+        print(f"Created date\t\t: {date.today()}")
+        print(f"Account holder's name\t: {account1.name}")
+        print(f"Account holder's age\t: {account1.age}")
+        print(f"Account holder's NIC\t: {account1.nic}")
+        print(f"Available balance\t: Rs.{account1.balance:.2f}")
+    except:
+        print("Invalid input. Please enter a valid initial deposit amount.")
+
+#Function to generate a random account number
 def account_number_generate():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     acc_num = ""
@@ -78,12 +129,14 @@ def account_number_generate():
         acc_num += new_char
     return acc_num
 
+#Function to find an account by account number
 def find_account(all_accounts, acc_num):
     for account in all_accounts:
         if account.account_number == acc_num:
             return account
     return None
 
+#Function to display the main menu
 def display_menu():
     print("\n\nE-CORP BANK MANAGEMENT SYSTEM")
     print("-------------------")
@@ -94,58 +147,15 @@ def display_menu():
     print("[5] Transfer Money")
     print("[6] Exit")
 
-
-# main function of the bank management system
+#Main function to run the program
 def main():
-    accounts_list = []
     while True:
         display_menu()
         choice = input("\nEnter a number between 1 to 6: ")
         
         match choice:
             case '1':
-                while True:
-                    name = input("\nEnter your name: ")
-                    if name:
-                        break
-                    else:
-                        print("Name can't be empty. Please try again.")
-                while True:
-                    age = input("Enter your age: ")
-                    if age:
-                        break
-                    else:
-                        print("Age can't be empty. Please try again.")
-                while True:
-                    nic = input("Enter your NIC number: ")
-                    if nic:
-                        break
-                    else:
-                        print("NIC number can't be empty. Please try again.")
-                try:
-                    while True:
-                        initial_deposit = float(input("Enter initial deposit amount: "))
-                        if initial_deposit >= 1000:
-                            break
-                        elif 0 < initial_deposit < 1000:
-                            print("Initial deposit amount must be at least Rs.1000.00. Please try again.")
-                        elif initial_deposit == 0:
-                            print("Initial deposit amount can't be zero. Please try again.")
-                        elif initial_deposit < 0:
-                            print("Initial deposit amount can't be negative. Please try again.")
-                    account_number = account_number_generate()
-                    account1 = BankAccount(account_number, name, age, nic, initial_deposit)
-                    accounts_list.append(account1)
-                    print(f"\nAccount created successfully. Your account number is {account1.account_number}.")
-                    print("\n---- New Account Details ----")
-                    print(f"Account number\t\t: {account1.account_number}")
-                    print(f"Created date\t\t: {date.today()}")
-                    print(f"Account holder's name\t: {account1.name}")
-                    print(f"Account holder's age\t: {account1.age}")
-                    print(f"Account holder's NIC\t: {account1.nic}")
-                    print(f"Available balance\t: Rs.{account1.balance:.2f}")
-                except:
-                    print("Invalid input. Please enter a valid initial deposit amount.")
+                new_account_create()
                 
             case '2':
                 account_number = input("\nEnter account number: ")
@@ -192,6 +202,8 @@ def main():
             case _:
                 print("\nInvalid choice. Please enter a number between 1 to 6.")
 
+#List to store all the bank accounts
+accounts_list = []
 
-# run the program by calling the main function
+#Run the main function
 main()
